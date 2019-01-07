a5c08fe515eb3908c7282c09c50c01a023140146
Santiago <santylore4@gmail.com>
2019-01-07 15:05:04 -0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import GoogleAuth from './GoogleAuth'
+ 
+export const Header = () => {
+  return (
+    <div className='ui scondary pointing menu'>
+      <Link to='/' className='item'>
+        Streamer
+      </Link>
+      <div className='right menu'>
+      <Link to='/' className='item'>
+        All Streams
+      </Link>
+      <GoogleAuth />
+      </div>
+    </div>
+  )
+}
